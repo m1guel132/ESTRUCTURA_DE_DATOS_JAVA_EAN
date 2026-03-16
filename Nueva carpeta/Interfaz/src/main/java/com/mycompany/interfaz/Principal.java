de52c1fc1492c0fa8743d7e18ecef95286a8254a
M1GUE7D <114818915+m1guel132@users.noreply.github.com>
2026-03-16 09:16:45 -0500

    Add files via upload

---
+++ Nueva carpeta/Interfaz/src/main/java/com/mycompany/interfaz/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfaz;
+
+/**
+ *
+ * @author SALA
+ */
+public class Interfaz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
